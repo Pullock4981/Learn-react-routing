b452608c88b25cbeda342cc1582f74af96678927
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2024-10-30 22:47:23 +0600

    add navigation

--- src/Components/Posts/PostDetails.jsx
+++ src/Components/Posts/PostDetails.jsx
@@ -1,15 +1,20 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 const PostDetails = () => {
-    const post = useLoaderData()
+    const navigate = useNavigate();
+    const post = useLoaderData();
     const { id, title, body} = post;
+    const handleNavigate = () =>{
+        navigate(-1);
+    }
     return (
         <div>
             <h3>Post detsils.....</h3>
             <p>Post ID: {id}</p>
             <h2>Post title: {title}</h2>
             <h4>{body}</h4>
+            <button onClick={handleNavigate}>Go back</button>
         </div>
     );
 };
